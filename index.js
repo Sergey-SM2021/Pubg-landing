bce3652fad0b624f6c5ddc965258d53cecb14b68
Sergey-SM2021 <84789763+Sergey-SM2021@users.noreply.github.com>
2022-05-25 21:52:24 +0300

    feat: adoptive header is ready

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 import './index.sass'
+
+const burger = document.querySelector('.burger')
+
+const nav = document.querySelector('.menu__list')
+
+const handlerBurgerClick = () => {
+    if (nav.classList.contains('active')) {
+        nav.classList.remove('active')
+        burger.classList.remove('open')
+    } else {
+        nav.classList.add('active')
+        burger.classList.add('open')
+    }
+}
+
+burger.addEventListener("click", handlerBurgerClick)
